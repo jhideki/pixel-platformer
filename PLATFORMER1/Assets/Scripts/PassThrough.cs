e1494dd0d8f74452767a35bef842e6bc02786ea1
sickkvnt69 <126116089+sickkvnt69@users.noreply.github.com>
2023-02-26 00:08:38 -0800

    Updated Camera and implemented falling through platforms

---
+++ PLATFORMER1/Assets/Scripts/PassThrough.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PassThrough : MonoBehaviour     
+{
+    private Collider2D platformCollider;
+    public bool onPlatform;
+
+    [SerializeField] private Object player;
+    private PlayerMovement2 movementScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformCollider = GetComponent<TilemapCollider2D>();
+        movementScript = player.GetComponent<PlayerMovement2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (onPlatform && movementScript.isCrouching)
+        {
+            platformCollider.enabled = false;
+            StartCoroutine(EnableCollider());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        onPlatform = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        onPlatform = false;
+    }
+
+    private IEnumerator EnableCollider()
+    {
+        
+        yield return new WaitForSeconds(0.2f);
+        
+        platformCollider.enabled = true;
+    }
+}

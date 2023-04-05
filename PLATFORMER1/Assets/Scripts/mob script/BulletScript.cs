5187f96a49840edfefee610841948d8887d8f88f
pgchmp22 <noaheh27@gmail.com>
2023-04-05 12:30:56 -0700

    mob mechanics

---
+++ PLATFORMER1/Assets/Scripts/mob script/BulletScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public GameObject enemy;
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+       
+       Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 180);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

8b7f59a778005da06f706e74d27805fabd306f58
Jonathan Ami <126116089+jonathan-ami@users.noreply.github.com>
2023-12-27 00:41:20 -0800

    implemented electricy trap
    - animations may need more work

---
+++ PLATFORMER1/Assets/Scripts/Traps/ElectricPlant.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricPlant : MonoBehaviour
+{
+    private enum MovementState {idle,start,maintain,end};
+    private Animator anim;
+    private BoxCollider2D collider;
+    [SerializeField] GameObject lightOff;
+    [SerializeField] GameObject lightOn;
+    [SerializeField] private float timeIdle;
+    [SerializeField] private float timeMaintain;
+    private bool isTimingIdle;
+    private bool isTimingMaintain;
+    private float startTimeIdle;
+    private float startTimeMaintain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      anim = GetComponent<Animator>();
+      collider = GetComponent<BoxCollider2D>();
+      MovementState state;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
+      if(currentState.IsName("Electric_Plant_Maintain"))
+      {
+        lightOff.SetActive(false);
+        lightOn.SetActive(true);
+
+      }else
+      {
+        lightOff.SetActive(true);
+        lightOn.SetActive(false);
+      }
+      if(!isTimingIdle)
+      {
+        startTimeIdle = Time.time;
+        isTimingIdle = true;
+        collider.enabled = false;
+      }
+
+      if((Time.time - startTimeIdle > timeIdle) && !isTimingMaintain)
+      {
+        anim.SetTrigger("start"); 
+        startTimeMaintain = Time.time;
+         Debug.Log(startTimeMaintain);
+         isTimingMaintain = true;
+         collider.enabled = true;
+       }
+
+       if((Time.time - startTimeMaintain > timeMaintain) && isTimingMaintain)
+       {
+         anim.SetTrigger("end");
+         isTimingIdle = false;
+         isTimingMaintain = false;
+       }
+    }
+}

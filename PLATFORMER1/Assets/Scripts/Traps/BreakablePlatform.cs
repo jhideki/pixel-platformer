bb73370eb691d45dcea066a033adc3bb2d569a25
Jonathan Ami <126116089+jonathan-ami@users.noreply.github.com>
2024-01-04 22:17:44 -0800

    working breakable platforms although still kinda buggy

--- PLATFORMER1/Assets/Scripts/Traps/BreakablePlatform.cs
+++ PLATFORMER1/Assets/Scripts/Traps/BreakablePlatform.cs
@@ -7,21 +7,27 @@ public class BreakablePlatform : MonoBehaviour
 
     [SerializeField] private Transform player;
     [SerializeField] private float duration;
+    [SerializeField] private float animOffset;
     private PlayerMovement2 playerScript;
     private bool startTimer;
     private float startTime;
     private BoxCollider2D[] colliders;
+    private Animator anim;
+    private bool isTiming;
+    
 
     void Start()
     {
         playerScript = player.GetComponent<PlayerMovement2>(); 
         colliders = GetComponents<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && playerScript.CanJump())
         {
+            Debug.Log("collided with player");
             startTimer = true;
         }
 
@@ -32,16 +38,20 @@ public class BreakablePlatform : MonoBehaviour
         {
             startTime = Time.time;
             startTimer = false;
+            isTiming = true;
         }
 
-        if(Time.time - startTime > duration)
+        if(Time.time - startTime > duration && isTiming)
         {
             GetComponent<Renderer>().enabled = false;
             foreach(var collider in colliders)
             {
                 collider.enabled = false;
             }
-
+        }
+        if(Time.time - startTime > animOffset && isTiming)
+        {
+            anim.SetTrigger("Break");
         }
 
     }
